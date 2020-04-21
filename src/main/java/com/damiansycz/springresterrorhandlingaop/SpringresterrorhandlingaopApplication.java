e85b90dc65335bb99760d9191b4f7c6c2e1aa573
damiansycz <damiansycz@outlook.com>
2020-04-21 12:39:14 +0200

    initial

---
+++ src/main/java/com/damiansycz/springresterrorhandlingaop/SpringresterrorhandlingaopApplication.java
@@ -0,0 +1,13 @@
+package com.damiansycz.springresterrorhandlingaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringresterrorhandlingaopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringresterrorhandlingaopApplication.class, args);
+    }
+
+}
